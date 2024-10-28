510b8f59fdcbc2a137b8af80870782c755a0ed1a
泡泡糖 <jinfengyao23@sina.com>
2024-10-29 00:39:00 +0800

    feat: 工程初始化

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # npm-ts-template
 ts开发npm包项目模版
+
+
+npx prettier . --check
+npx prettier . --write
---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+
+export default [
+  {
+    files: ['src/**/*.{js,mjs,cjs,ts}'],
+    ignores: ['*.config.js'],
+  },
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+]
